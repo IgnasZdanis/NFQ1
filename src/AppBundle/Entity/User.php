fab32b620cff1cca177ca65085f17b5a97f3d037
IgnasZdanis <zdanis.ignas@gmail.com>
2017-10-13 21:59:21 +0300

    Installed FOSUserBundle and created User class. Updated config and security files for use with FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ignas
+ * Date: 17.10.13
+ * Time: 12.55
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+}
