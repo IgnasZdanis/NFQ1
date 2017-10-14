b1586f4d1f95e4da3fa0d6400df3799f26e685af
IgnasZdanis <zdanis.ignas@gmail.com>
2017-10-14 16:21:36 +0300

    Overwriting FOSUserBundle default layout and content

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,26 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @throws \InvalidArgumentException
      */
     public function indexAction(Request $request)
     {
+        $url = $this->generateUrl('about_page');
+        /*
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $url = $this->generateUrl('');
+        }
+        */
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $response = new RedirectResponse($url);
+        return $response;
+
     }
 }
