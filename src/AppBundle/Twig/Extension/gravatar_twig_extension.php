1fe63fca042cb593fcf560c265f144e754ef2e34
IgnasZdanis <zdanis.ignas@gmail.com>
2017-10-14 15:16:21 +0300

    Gravatar_twix_extension completed, added log out button in /about

--- src/AppBundle/Twig/Extension/gravatar_twig_extension.php
+++ src/AppBundle/Twig/Extension/gravatar_twig_extension.php
@@ -8,8 +8,10 @@ class gravatar_twig_extension extends \Twig_Extension
 {
     public function getFilters()
     {
-        $filter = new Twig_SimpleFilter('gravatar', function ($string) {
-            return "gravatar works";
+        $filter = new Twig_SimpleFilter('gravatar', function ($email) {
+            $email = trim($email);
+            $email = strtolower($email);
+            return "https://www.gravatar.com/avatar/" . md5( $email );
         });
         return [
             $filter
