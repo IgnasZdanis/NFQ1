1db66b617c6863d5083c9c9fb750c3d62a1823b3
IgnasZdanis <zdanis.ignas@gmail.com>
2017-10-14 14:26:48 +0300

    redirecting to /login if user is not logged in, showing username in about

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -10,10 +10,16 @@ class ProfileController extends Controller
 {
 
     /**
-     * @Route("/about")
+     * @Route("/about", name="about_page")
      */
     public function aboutAction(Request $request)
     {
+        /*
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $url = $this->generateUrl('');
+        }
+        */
+        //$username = $request->getUser();
         return $this->render('AppBundle::about.html.twig');
     }
 }
